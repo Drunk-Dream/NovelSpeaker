c054857bd0b00a925aca73204e77347a6f2bc88e
Drunk-Dream <dongmh3@outlook.com>
2026-06-18 20:44:54 +0800

    build: add layered solution skeleton

---
+++ src/NovelSpeaker.Domain/Common/AppInfo.cs
@@ -0,0 +1,9 @@
+namespace NovelSpeaker.Domain.Common;
+
+/// <summary>
+/// Provides application-wide constants shared by startup components.
+/// </summary>
+public static class AppInfo
+{
+    public const string ProductName = "NovelSpeaker";
+}
