d2d9b2f3b1a36d09ba3792557a59f3c96b49d73a
Drunk-Dream <dongmh3@outlook.com>
2026-06-25 17:28:06 +0800

    test(playback): extend fake wave player for stale callback simulation

--- tests/NovelSpeaker.UnitTests/Playback/FakeWavePlayer.cs
+++ tests/NovelSpeaker.UnitTests/Playback/FakeWavePlayer.cs
@@ -4,6 +4,8 @@ namespace NovelSpeaker.UnitTests.Playback;
 
 internal sealed class FakeWavePlayer : IWavePlayer
 {
+    private EventHandler<StoppedEventArgs>? _playbackStopped;
+
     public PlaybackState PlaybackState { get; private set; } = PlaybackState.Stopped;
 
     public WaveFormat? OutputWaveFormat => WaveProvider?.WaveFormat;
@@ -12,7 +14,11 @@ internal sealed class FakeWavePlayer : IWavePlayer
 
     public IWaveProvider? WaveProvider { get; private set; }
 
-    public event EventHandler<StoppedEventArgs>? PlaybackStopped;
+    public event EventHandler<StoppedEventArgs>? PlaybackStopped
+    {
+        add => _playbackStopped += value;
+        remove => _playbackStopped -= value;
+    }
 
     public void Init(IWaveProvider waveProvider)
     {
@@ -32,13 +38,24 @@ internal sealed class FakeWavePlayer : IWavePlayer
     public void Stop()
     {
         PlaybackState = PlaybackState.Stopped;
-        PlaybackStopped?.Invoke(this, new StoppedEventArgs());
+        _playbackStopped?.Invoke(this, new StoppedEventArgs());
     }
 
     public void RaisePlaybackStopped(Exception? exception = null)
     {
         PlaybackState = PlaybackState.Stopped;
-        PlaybackStopped?.Invoke(this, new StoppedEventArgs(exception));
+        _playbackStopped?.Invoke(this, new StoppedEventArgs(exception));
+    }
+
+    public EventHandler<StoppedEventArgs>? CapturePlaybackStoppedHandlers()
+    {
+        return _playbackStopped;
+    }
+
+    public void RaiseCapturedPlaybackStopped(EventHandler<StoppedEventArgs>? handlers, Exception? exception = null)
+    {
+        PlaybackState = PlaybackState.Stopped;
+        handlers?.Invoke(this, new StoppedEventArgs(exception));
     }
 
     public void Dispose()
